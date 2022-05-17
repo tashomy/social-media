97a5fab5d7f2a2ffd819181931c8890c535d8a57
Natasa <mijatovicn650@gmail.com>
2022-05-17 11:47:58 +0200

    basic folder structure, starting jsx and styles are implemented

---
+++ client/src/components/Posts/Posts.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Post from "./Post/Post";
+import useStyles from "./styles";
+
+const Posts = () => {
+  const classes = useStyles();
+
+  return (
+    <div>
+      Posts <Post />
+      <Post />
+    </div>
+  );
+};
+
+export default Posts;
